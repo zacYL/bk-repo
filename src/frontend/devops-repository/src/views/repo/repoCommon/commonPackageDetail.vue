e1c70d1252eb54ad28680b8dbfaa997d4511735b
陈炯明 <eugene@canway.net>
2021-06-07 16:33:54 +0800

    fix: 隐藏report/log仓库&custom/pipeline仓库显示名称优化 issue #465

--- src/frontend/devops-repository/src/views/repo/repoCommon/commonPackageDetail.vue
+++ src/frontend/devops-repository/src/views/repo/repoCommon/commonPackageDetail.vue
@@ -160,7 +160,7 @@
                 pagination: {
                     count: 0,
                     current: 1,
-                    limit: 10,
+                    limit: 20,
                     'limit-list': [10, 20, 40]
                 }
             }

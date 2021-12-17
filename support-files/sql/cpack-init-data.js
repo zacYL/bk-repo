0c769a7f2249aa64ff0911c6d992c5b6888f63fb
陈灿 <charis@canway.net>
2021-12-17 16:24:28 +0800

    feature：cpack独立部署初始化项目和管理员用户

---
+++ support-files/sql/cpack-init-data.js
@@ -0,0 +1,37 @@
+//初始管理员账号，userId:admin , password：bkrepo
+db.user.updateOne(
+    { userId: "admin" },
+    {
+        $setOnInsert: {
+            userId: "admin",
+            name: "admin",
+            pwd: "ed424c8ee8955e30b9975ed83fdfef61",
+            admin: true,
+            locked: false,
+            tokens: [],
+            roles: [],
+            asstUsers: [],
+            group: false,
+            createdDate: new Date(),
+            lastModifiedDate: new Date()
+        }
+    },
+    { upsert: true }
+);
+
+// 初始化demo项目
+db.project.updateOne(
+    { name: "demo" },
+    {
+        $setOnInsert: {
+            name: "demo",
+            displayName: "demo",
+            description: "示例项目",
+            createdBy: "admin",
+            createdDate: new Date(),
+            lastModifiedBy: "admin",
+            lastModifiedDate: new Date()
+        }
+    },
+    { upsert: true }
+);

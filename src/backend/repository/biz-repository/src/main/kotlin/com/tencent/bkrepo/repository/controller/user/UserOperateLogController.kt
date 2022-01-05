3d47ed8aa5e8b907e3e7f65e4e8d9a164e0befcb
weaving <weaving@canway.net>
2022-01-05 15:21:23 +0800

    refactor: 审计日志接口调整

--- src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/controller/user/UserOperateLogController.kt
+++ src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/controller/user/UserOperateLogController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/operate/log")
+@RequestMapping("/api/log")
 class UserOperateLogController(
     private val operateLogService: OperateLogService
 ) {

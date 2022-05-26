1537f6d250a43fa8b7f3053fd4d01854d8521bce
陈炯明 <eugene@canway.net>
2022-05-26 17:02:16 +0800

    fix: p1_0 屏蔽没有自动清理功能的仓库的页面入口

--- src/frontend/devops-repository/src/views/repoConfig/index.vue
+++ src/frontend/devops-repository/src/views/repoConfig/index.vue
@@ -79,7 +79,7 @@
             <bk-tab-panel v-if="showProxyConfigTab" name="proxyConfig" :label="$t('proxyConfig')">
                 <proxy-config :base-data="repoBaseInfo" @refresh="getRepoInfoHandler"></proxy-config>
             </bk-tab-panel>
-            <bk-tab-panel render-directive="if" name="cleanConfig" label="清理设置">
+            <bk-tab-panel v-if="showCleanConfigTab" render-directive="if" name="cleanConfig" label="清理设置">
                 <clean-config :base-data="repoBaseInfo" @refresh="getRepoInfoHandler"></clean-config>
             </bk-tab-panel>
             <bk-tab-panel render-directive="if" name="permissionConfig" :label="$t('permissionConfig')">
@@ -185,6 +185,9 @@
             },
             showProxyConfigTab () {
                 return ['maven', 'pypi', 'npm', 'composer', 'nuget'].includes(this.repoType)
+            },
+            showCleanConfigTab () {
+                return ['maven', 'docker', 'npm', 'helm', 'generic'].includes(this.repoType)
             },
             repoAddress () {
                 const { repoType, name } = this.repoBaseInfo

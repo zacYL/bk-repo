835190c61e3da03310dfdb875f46b9b19b46c06b
xujian <xujian@canway.net>
2022-05-31 15:33:21 +0800

    bugfix: p1_17238 【制品库独立部署】登录失败问题修复

--- src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/util/RsaUtils.kt
+++ src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/util/RsaUtils.kt
@@ -1,29 +1,67 @@
 package com.tencent.bkrepo.auth.util
 
+import cn.hutool.crypto.CryptoException
 import cn.hutool.crypto.asymmetric.KeyType
 import cn.hutool.crypto.asymmetric.RSA
+import com.tencent.bkrepo.auth.constant.PRIVATE_KEY
+import com.tencent.bkrepo.auth.constant.PUBLIC_KEY
+import com.tencent.bkrepo.common.redis.RedisOperation
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
 
-/**
- * RSA 非对称加密工具类
- */
-object RsaUtils {
-    private val rsa = RSA()
-    var privateKey = rsa.privateKeyBase64!!
-    var publicKey = rsa.publicKeyBase64!!
+@Component
+class RsaUtils(
+    private val redisOperation: RedisOperation
+){
+    /**
+     * 生成密钥对，并存入redis
+     */
+    fun generateRsa() {
+        val rsa = RSA()
+        redisOperation.set(PRIVATE_KEY, rsa.privateKeyBase64)
+        redisOperation.set(PUBLIC_KEY, rsa.publicKeyBase64)
+    }
+
+    /**
+     * 从redis获取公钥
+     */
+    fun getPublicKey(): String {
+        if (redisOperation.get(PUBLIC_KEY) == null) {
+            logger.info("public key is null,will to generate rsa")
+            generateRsa()
+        }
+        return redisOperation.get(PUBLIC_KEY)!!
+    }
+
+    /**
+     * 从redis获取私钥
+     */
+    fun getPrivateKey(): String {
+//        if (redisOperation.get(PRIVATE_KEY) == null) {
+//            logger.info("private key is null,will to generate rsa")
+//            generateRsa()
+//        }
+        return redisOperation.get(PRIVATE_KEY)!!
+    }
 
     /**
      * 公钥加密
-     * @param password 需要解密的密码
      */
-    fun encrypt(password: String): String {
+    fun encrypt(password: String, publicKey: String): String {
+        val rsa = RSA(null, publicKey)
         return rsa.encryptBcd(password, KeyType.PublicKey)
     }
 
     /**
-     * 私钥解密，返回解密后的密码
-     * @param password 前端加密后的密码
+     * 私钥解密
      */
     fun decrypt(password: String): String {
+        val privateKey = redisOperation.get(PRIVATE_KEY) ?: throw CryptoException("private key is null")
+        val rsa = RSA(privateKey, null)
         return rsa.decryptStr(password, KeyType.PrivateKey)
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(RsaUtils::class.java)
     }
 }

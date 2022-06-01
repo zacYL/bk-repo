29d4f52abbd78cb84ed774a837ba53e9d1acc30b
weaving <weaving@canway.net>
2022-06-01 18:26:27 +0800

    feature: p1_0000 dependency-checker 版本升级

--- src/backend/common/common-checker/biz-checker/build.gradle.kts
+++ src/backend/common/common-checker/biz-checker/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     api(project(":common:common-checker:api-checker"))
-    api("net.canway:dependency-check-simple:0.2.3") {
+    api("net.canway:dependency-check-simple:0.3.1") {
         exclude(group = "junit", module = "junit")
     }
 }

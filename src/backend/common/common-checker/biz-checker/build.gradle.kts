1a6ceea3c96efae385b62a15918af78c4e51d11b
weaving <weaving@canway.net>
2022-06-06 18:24:53 +0800

    feature: p1_0000 dependency-checker升级至0.4.0

--- src/backend/common/common-checker/biz-checker/build.gradle.kts
+++ src/backend/common/common-checker/biz-checker/build.gradle.kts
@@ -1,6 +1,8 @@
 dependencies {
     api(project(":common:common-checker:api-checker"))
-    api("net.canway:dependency-check-simple:0.3.1") {
+    implementation("net.canway:dependency-check-simple:0.4.0") {
         exclude(group = "junit", module = "junit")
+        exclude(group = "org.slf4j", module = "slf4j-api")
+        exclude(group = "org.slf4j", module = "slf4j-simple")
     }
 }

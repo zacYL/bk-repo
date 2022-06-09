9cb9bde6e03377be136c2fefe42877ee75ba3502
eric <scplsy@gmail.com>
2022-06-09 22:22:46 +0800

    refactor: p1_17492 对接新版许可SDK

--- src/backend/common/common-license-canway/src/main/kotlin/net/devops/canway/common/lse/feign/FeignConfiguration.kt
+++ src/backend/common/common-license-canway/src/main/kotlin/net/devops/canway/common/lse/feign/FeignConfiguration.kt
@@ -2,6 +2,7 @@ package net.devops.canway.common.lse.feign
 
 //import com.fasterxml.jackson.databind.ObjectMapper
 //import com.tencent.bkrepo.common.service.feign.ErrorCodeDecoder
+import feign.Client
 import feign.Contract
 //import feign.RequestInterceptor
 import feign.codec.Decoder
@@ -10,13 +11,20 @@ import feign.codec.Encoder
 import feign.jackson.JacksonDecoder
 import feign.jackson.JacksonEncoder
 import feign.jaxrs.JAXRSContract
+import okhttp3.OkHttpClient
 import org.springframework.context.annotation.Bean
+import java.util.concurrent.TimeUnit
 
 class FeignConfiguration(
 //    private val objectMapper: ObjectMapper,
 //    private val errorCodeDecoder: ErrorCodeDecoder,
 //    private val requestInterceptor: RequestInterceptor
 ) {
+    @Bean
+    fun client(): Client {
+        return feign.okhttp.OkHttpClient(okHttpClient)
+    }
+
     @Bean
     fun contract(): Contract {
         return JAXRSContract()
@@ -31,6 +39,17 @@ class FeignConfiguration(
     fun decoder(): Decoder {
         return JacksonDecoder()
     }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .sslSocketFactory(
+            CertTrustManager.disableValidationSSLSocketFactory,
+            CertTrustManager.disableValidationTrustManager
+        )
+        .hostnameVerifier(CertTrustManager.trustAllHostname)
+        .connectTimeout(3L, TimeUnit.SECONDS)
+        .readTimeout(5L, TimeUnit.SECONDS)
+        .writeTimeout(5L, TimeUnit.SECONDS)
+        .build()
 
 //    @Bean
 //    fun errorDecoder(): ErrorDecoder {

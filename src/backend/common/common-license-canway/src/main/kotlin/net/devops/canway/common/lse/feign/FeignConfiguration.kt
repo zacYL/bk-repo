29e5fa848e44eda1dd4561003353917b46da5590
eric <scplsy@gmail.com>
2022-06-09 21:21:30 +0800

    refactor: p1_17492 对接新版许可SDK

--- src/backend/common/common-license-canway/src/main/kotlin/net/devops/canway/common/lse/feign/FeignConfiguration.kt
+++ src/backend/common/common-license-canway/src/main/kotlin/net/devops/canway/common/lse/feign/FeignConfiguration.kt
@@ -1,21 +1,21 @@
 package net.devops.canway.common.lse.feign
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.tencent.bkrepo.common.service.feign.ErrorCodeDecoder
+//import com.fasterxml.jackson.databind.ObjectMapper
+//import com.tencent.bkrepo.common.service.feign.ErrorCodeDecoder
 import feign.Contract
-import feign.RequestInterceptor
+//import feign.RequestInterceptor
 import feign.codec.Decoder
 import feign.codec.Encoder
-import feign.codec.ErrorDecoder
+//import feign.codec.ErrorDecoder
 import feign.jackson.JacksonDecoder
 import feign.jackson.JacksonEncoder
 import feign.jaxrs.JAXRSContract
 import org.springframework.context.annotation.Bean
 
 class FeignConfiguration(
-    private val objectMapper: ObjectMapper,
-    private val errorCodeDecoder: ErrorCodeDecoder,
-    private val requestInterceptor: RequestInterceptor
+//    private val objectMapper: ObjectMapper,
+//    private val errorCodeDecoder: ErrorCodeDecoder,
+//    private val requestInterceptor: RequestInterceptor
 ) {
     @Bean
     fun contract(): Contract {
@@ -24,21 +24,21 @@ class FeignConfiguration(
 
     @Bean
     fun encoder(): Encoder {
-        return JacksonEncoder(objectMapper)
+        return JacksonEncoder()
     }
 
     @Bean
     fun decoder(): Decoder {
-        return JacksonDecoder(objectMapper)
+        return JacksonDecoder()
     }
 
-    @Bean
-    fun errorDecoder(): ErrorDecoder {
-        return errorCodeDecoder
-    }
+//    @Bean
+//    fun errorDecoder(): ErrorDecoder {
+//        return errorCodeDecoder
+//    }
 
-    @Bean
-    fun requestInterceptor(): RequestInterceptor {
-        return requestInterceptor
-    }
+//    @Bean
+//    fun requestInterceptor(): RequestInterceptor {
+//        return requestInterceptor
+//    }
 }

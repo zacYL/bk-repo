a2fe287672071e0b5a35a7d61a55a74c7fb93eca
eric <scplsy@gmail.com>
2022-06-09 19:53:14 +0800

    refactor: p1_17492 对接新版许可SDK

--- src/backend/common/common-license-canway/build.gradle.kts
+++ src/backend/common/common-license-canway/build.gradle.kts
@@ -4,5 +4,6 @@ dependencies {
     implementation(project(":common:common-service"))
     implementation(project(":common:common-api"))
     implementation("net.canway.license:sdk-license:4.3.10-platform")
+    api("io.github.openfeign:feign-jackson")
     api("io.github.openfeign:feign-jaxrs")
 }
--- src/backend/common/common-license-canway/src/main/kotlin/net/devops/canway/common/lse/feign/FeignConfiguration.kt
+++ src/backend/common/common-license-canway/src/main/kotlin/net/devops/canway/common/lse/feign/FeignConfiguration.kt
@@ -1,12 +1,44 @@
 package net.devops.canway.common.lse.feign
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.tencent.bkrepo.common.service.feign.ErrorCodeDecoder
 import feign.Contract
+import feign.RequestInterceptor
+import feign.codec.Decoder
+import feign.codec.Encoder
+import feign.codec.ErrorDecoder
+import feign.jackson.JacksonDecoder
+import feign.jackson.JacksonEncoder
 import feign.jaxrs.JAXRSContract
 import org.springframework.context.annotation.Bean
 
-class FeignConfiguration {
+class FeignConfiguration(
+    private val objectMapper: ObjectMapper,
+    private val errorCodeDecoder: ErrorCodeDecoder,
+    private val requestInterceptor: RequestInterceptor
+) {
     @Bean
     fun contract(): Contract {
         return JAXRSContract()
+    }
+
+    @Bean
+    fun encoder(): Encoder {
+        return JacksonEncoder(objectMapper)
+    }
+
+    @Bean
+    fun decoder(): Decoder {
+        return JacksonDecoder(objectMapper)
+    }
+
+    @Bean
+    fun errorDecoder(): ErrorDecoder {
+        return errorCodeDecoder
+    }
+
+    @Bean
+    fun requestInterceptor(): RequestInterceptor {
+        return requestInterceptor
     }
 }

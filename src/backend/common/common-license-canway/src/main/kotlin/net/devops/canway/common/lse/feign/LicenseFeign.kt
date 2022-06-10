83047153bc0e3e978345626da0ad865e9901faf5
eric <scplsy@gmail.com>
2022-06-10 11:25:36 +0800

    refactor: p1_17492 对接新版许可SDK

--- src/backend/common/common-license-canway/src/main/kotlin/net/devops/canway/common/lse/feign/LicenseFeign.kt
+++ src/backend/common/common-license-canway/src/main/kotlin/net/devops/canway/common/lse/feign/LicenseFeign.kt
@@ -1,10 +1,9 @@
 package net.devops.canway.common.lse.feign
 
-import com.tencent.bkrepo.common.api.constant.LICENSE_SERVICE_NAME
 import net.canway.license.api.LicenseApi
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.stereotype.Service
 
 @Service
-@FeignClient(LICENSE_SERVICE_NAME, configuration = [FeignConfiguration::class], contextId = "LicenseFeign")
+@FeignClient("license-devops", configuration = [FeignConfiguration::class], contextId = "LicenseFeign")
 interface LicenseFeign : LicenseApi

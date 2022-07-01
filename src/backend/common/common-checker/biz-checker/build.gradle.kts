11a505eadfd24ef5b6d12b749ec1978fca2bae32
weaving <weaving@canway.net>
2022-07-01 16:24:52 +0800

    refactor: p1_0000 dependencycheck 升级至0.5.2, 修复扫描结果为空的问题

--- src/backend/common/common-checker/biz-checker/build.gradle.kts
+++ src/backend/common/common-checker/biz-checker/build.gradle.kts
@@ -27,7 +27,7 @@
 
 dependencies {
     api(project(":common:common-checker:api-checker"))
-    implementation("net.canway:dependency-check-simple:0.5.1-SNAPSHOT") {
+    implementation("net.canway:dependency-check-simple:0.5.2") {
         exclude(group = "junit", module = "junit")
         exclude(group = "org.slf4j", module = "slf4j-api")
         exclude(group = "org.slf4j", module = "slf4j-simple")

a288ad8f9d70042e0738c3a6e89cb1c5741ca444
carroll <carroll@canway.net>
2022-07-05 11:23:49 +0800

    fix:p1_18601 【CPack】制品扫描质量规则状态优化

--- src/backend/scanner/biz-scanner/src/main/kotlin/com/tencent/bkrepo/scanner/task/DefaultScanTaskScheduler.kt
+++ src/backend/scanner/biz-scanner/src/main/kotlin/com/tencent/bkrepo/scanner/task/DefaultScanTaskScheduler.kt
@@ -369,7 +369,7 @@ class DefaultScanTaskScheduler @Autowired constructor(
                 ?.overview
                 ?.let { Converter.convert(it) }
             // 质量检查结果
-            val qualityPass = if (qualityRule != null && overview != null) {
+            val qualityPass = if (!qualityRule.isNullOrEmpty() && overview != null) {
                 scanQualityService.checkScanQualityRedLine(qualityRule, overview)
             } else {
                 null

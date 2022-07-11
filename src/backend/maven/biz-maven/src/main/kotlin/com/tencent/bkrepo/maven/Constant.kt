1fba45f46f134358c1ced506bc914ce63325b0cd
weaving <weaving@canway.net>
2022-07-11 14:53:23 +0800

    feature: p1_18749 maven aar格式制品支持

--- src/backend/maven/biz-maven/src/main/kotlin/com/tencent/bkrepo/maven/Constant.kt
+++ src/backend/maven/biz-maven/src/main/kotlin/com/tencent/bkrepo/maven/Constant.kt
@@ -1,5 +1,5 @@
 package com.tencent.bkrepo.maven
 
 const val PACKAGE_SUFFIX_REGEX =
-    "(.)+-(.)+\\.(jar|war|tar|ear|ejb|rar|msi|rpm|tar\\.bz2|tar\\.gz|tar\\.xz|tbz|zip|pom)\$"
+    "(.)+-(.)+\\.(jar|war|tar|ear|ejb|rar|msi|rpm|tar\\.bz2|tar\\.gz|tar\\.xz|tbz|zip|pom|aar)\$"
 const val SNAPSHOT_SUFFIX = "-SNAPSHOT"

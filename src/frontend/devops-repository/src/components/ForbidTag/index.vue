55514d0fbf719a006e0b09073954398bbe5f0e91
king <pengkaiwang@canway.net>
2022-08-25 11:01:52 +0800

    fix: p1_0 仓库列表-禁止使用-tag QUALITYUNPASS 状态 tooltips信息不展示问题

--- src/frontend/devops-repository/src/components/ForbidTag/index.vue
+++ src/frontend/devops-repository/src/components/ForbidTag/index.vue
@@ -13,10 +13,11 @@
         computed: {
             ...mapState(['userList']),
             tooltipContent () {
+                console.log('this.forbidType :>> ', this.forbidType)
                 switch (this.forbidType) {
                     case 'SCANNING':
                         return '制品正在扫描中'
-                    case 'QUALITYUNPASS':
+                    case 'QUALITY_UNPASS':
                         return '制品扫描质量规则未通过'
                     case 'MANUAL':
                         return `${this.userList[this.forbidUser]?.name || this.forbidUser} 手动禁止`

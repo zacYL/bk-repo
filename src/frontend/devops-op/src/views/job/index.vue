2188aef1b4799daa6cfd406208a522047c0990ce
v_wuqdwu <v_wuqdwu@tencent.com>
2022-09-20 17:27:13 +0800

    --story=877551695 job模块的 执行作业能通过op平台管理
    (cherry picked from commit 09a4d749af2eaacfe77a64e5fb730c558fe3d6d6)

--- src/frontend/devops-op/src/views/job/index.vue
+++ src/frontend/devops-op/src/views/job/index.vue
@@ -104,12 +104,18 @@ export default {
       update(job.name, job.enabled, job.runnig).then(() => {
         this.query()
       })
-      const key = 'job.' + job.name + '.enabled'
+      let jobName;
+      if (job.name.endsWith('Job')) {
+        jobName = job.name.substr(0, job.name.length - 3)
+      } else {
+        jobName = job.name
+      }
+      const key = 'job.' + jobName + '.enabled'
       const values = [{
         'key': key,
         'value': job.enabled
       }]
-      updateConfig(values)
+      updateConfig(values, 'job')
     },
     changeRunning(job) {
       update(job.name, job.enabled, !job.runnig).then(() => {

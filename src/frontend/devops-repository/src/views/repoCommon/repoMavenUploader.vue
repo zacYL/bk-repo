e0839549e48383ac1fdb0427ca6939a42b52601b
king <pengkaiwang@canway.net>
2022-11-01 10:08:26 +0800

    feat: p1_0 增加maven仓库上传面板

---
+++ src/frontend/devops-repository/src/views/repoCommon/repoMavenUploader.vue
@@ -0,0 +1,45 @@
+<template>
+    <bk-sideslider
+        :is-show.sync="isVisible"
+        :quick-close="true"
+        :width="600"
+        @hidden="handleClickClose"
+    >
+        <div slot="header">{{ customSettings.title }}</div>
+        <div class="p20" slot="content">
+            自定义内容
+        </div>
+    </bk-sideslider>
+</template>
+
+<script>
+    export default {
+        model: {
+            prop: 'isVisible',
+            event: 'update'
+        },
+        props: {
+            isVisible: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data () {
+            return {
+                customSettings: {
+                    isShow: false,
+                    title: '更多参数配置'
+                }
+            }
+        },
+        methods: {
+            handleClickClose () {
+                this.$emit('update', false)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

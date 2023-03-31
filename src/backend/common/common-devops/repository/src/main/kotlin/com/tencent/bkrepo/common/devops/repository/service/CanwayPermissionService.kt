2f5d3f20a574c671065fcf34581bb98a3bad3b11
禤嘉俊 <ericxuan@canway.net>
2023-03-31 14:20:18 +0800

    feat:p1_26289 对接新权限中心接口

--- src/backend/common/common-devops/repository/src/main/kotlin/com/tencent/bkrepo/common/devops/repository/service/CanwayPermissionService.kt
+++ src/backend/common/common-devops/repository/src/main/kotlin/com/tencent/bkrepo/common/devops/repository/service/CanwayPermissionService.kt
@@ -3,6 +3,7 @@ package com.tencent.bkrepo.common.devops.repository.service
 import com.tencent.bkrepo.common.api.util.readJsonString
 import com.tencent.bkrepo.common.api.util.toJsonString
 import com.tencent.bkrepo.common.devops.CANWAY_AUTH_API
+import com.tencent.bkrepo.common.devops.RESOURCECODE
 import com.tencent.bkrepo.common.devops.conf.DevopsConf
 import com.tencent.bkrepo.common.devops.enums.CanwayPermissionType
 import com.tencent.bkrepo.common.devops.pojo.request.CanwayPermissionRequest
@@ -48,13 +49,13 @@ class CanwayPermissionService(
     ): Boolean? {
         val canwayCheckPermissionRequest = CanwayPermissionRequest(
             userId = userId,
-            instanceId = repoName,
-            resourceCode = resourceCode,
+            instanceId = repoName ?: "*",
+            resourceCode = resourceCode ?: RESOURCECODE,
             actionCodes = listOf(action.value),
         ).toJsonString()
-        val ciAddResourceUrl = String.format("${devopsHost.removeSuffix("/")}$CANWAY_AUTH_API$ciCheckPermissionApi",projectId)
+        val ciAddResourceUrl =
+            String.format("${devopsHost.removeSuffix("/")}$CANWAY_AUTH_API$ciCheckPermissionApi", projectId)
         val responseContent = SimpleHttpUtils.doPost(ciAddResourceUrl, canwayCheckPermissionRequest).content
-        logger.info("打印返回体:$responseContent")
         return responseContent.readJsonString<CanwayResponse<Boolean>>().data
     }
 

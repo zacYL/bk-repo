bbced91f65960b604a8adb1bbef90da9ffb85f8e
xujian <xujian@canway.net>
2023-04-27 09:49:36 +0800

    perf:p1_27208 分发中项目名称与项目标识的连接符优化

--- src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/replica/base/replicator/ClusterReplicator.kt
+++ src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/replica/base/replicator/ClusterReplicator.kt
@@ -80,7 +80,7 @@ class ClusterReplicator(
             // 外部集群仓库没有project/repoName
             if (remoteProjectId.isNullOrBlank()) return
             val localProject = localDataManager.findProjectById(localProjectId)
-            val combineName = "${localProject.displayName}_$remoteProjectId"
+            val combineName = "${localProject.displayName}---$remoteProjectId"
             val displayName = if (combineName.length > 100) combineName.substring(0, 100) else combineName
             val request = ProjectCreateRequest(
                 name = remoteProjectId,

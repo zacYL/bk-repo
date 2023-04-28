07cc8d072194576cdc48ba734d289aa8268ca6a2
谷俊崎[junqigu] <junqigu@canway.net>
2023-04-28 17:31:50 +0800

    feat: p1_27513 【CPack】generic仓库上传文件弹窗及文件展示列表上方面包屑样式优化

--- src/frontend/devops-repository/src/components/Breadcrumb/index.vue
+++ src/frontend/devops-repository/src/components/Breadcrumb/index.vue
@@ -3,7 +3,8 @@
         <slot></slot>
         <bk-breadcrumb-item
             v-for="item in list"
-            :key="item.name">
+            :key="item.name"
+            :class="{ 'omit-middle': omitMiddle && list && list.length > 2 }">
             <span class="bk-breadcrumb-item-inner is-link"
                 @click="() => item.cilckHandler && item.cilckHandler(item)">
                 {{ item.name }}
@@ -18,7 +19,24 @@
             list: {
                 type: Array,
                 default: () => []
+            },
+            // 是否需要在面包屑超过两个之后只显示首尾，中间显示省略号
+            omitMiddle: {
+                type: Boolean,
+                default: false
             }
         }
     }
 </script>
+<style lang="scss" scoped>
+.bk-breadcrumb-item.omit-middle{
+    &:not(:first-child) {
+        &:not(:last-child){
+            display: none !important;
+        }
+    }
+    &:first-child:after{
+        content: '...>';
+    }
+}
+</style>

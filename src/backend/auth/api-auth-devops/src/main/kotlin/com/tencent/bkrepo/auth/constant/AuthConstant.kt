32f852cd84bdb6aeca2befa6f8a24af78fa8e582
禤嘉俊[ericxuan] <ericxuan@canway.net>
2023-07-03 19:57:32 +0800

    feat: p230_70 集成平台认证与增加token迁移接口

--- src/backend/auth/api-auth-devops/src/main/kotlin/com/tencent/bkrepo/auth/constant/AuthConstant.kt
+++ src/backend/auth/api-auth-devops/src/main/kotlin/com/tencent/bkrepo/auth/constant/AuthConstant.kt
@@ -3,25 +3,13 @@ package com.tencent.bkrepo.auth.constant
 object AuthConstant {
     const val ANY_RESOURCE_CODE = "*"
 
-    const val X_DEVOPS_TOKEN = "X-DEVOPS-TOKEN"
-
-    const val USER_TOKEN_REDIS_PREFIX = "devops:auth:user:"
-
-    const val TOKEN_USER_REDIS_PREFIX = "devops:auth:token:"
-
-    const val LOGIN_TOKEN_USER_REDIS_PREFIX = "devops:auth:login:token:"
-
-    const val DEVOPS_TOKEN_LOCK = "devops_token_lock_"
-
-    const val DEVOPS_TENANT = "devops_tenant_"
-
-    const val DEVOPS_TOKEN = "devops_token_"
-
     const val TOKEN_EXPIRE_TIME_IN_SECOND = 2 * 60 * 60L
 
     const val TOKEN_EXPIRE_TIME_IN_MS = TOKEN_EXPIRE_TIME_IN_SECOND * 1000L
 
     const val DEVOPS_AUTH_NAME = "\${service.devops-prefix:}auth\${service.suffix:}"
+
+    const val DEVOPS_USER_NAME = "\${service.devops-prefix:}usermanager\${service.suffix:}"
 
     const val AUTH_HEADER_USER_ID = "X-DEVOPS-UID"
 
---
+++ src/backend/auth/biz-auth-devops/src/main/kotlin/com/tencent/bkrepo/auth/controller/user/ExtTokenController.kt
@@ -0,0 +1,21 @@
+package com.tencent.bkrepo.auth.controller.user
+
+import com.tencent.bkrepo.auth.service.impl.ExtTokenServiceImpl
+import com.tencent.bkrepo.common.security.permission.Principal
+import com.tencent.bkrepo.common.security.permission.PrincipalType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/token")
+class ExtTokenController(
+    private val extTokenServiceImpl: ExtTokenServiceImpl
+) {
+
+    @Principal(PrincipalType.ADMIN)
+    @GetMapping("/migrate")
+    fun migrate() {
+        extTokenServiceImpl.migHistoryTokenData()
+    }
+}

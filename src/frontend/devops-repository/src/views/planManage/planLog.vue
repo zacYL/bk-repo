b85efda132936447bc4f34b2ee2bd1842d02e225
谷俊崎[junqigu] <junqigu@canway.net>
2023-07-07 17:46:25 +0800

    feat: p230_277 农信分发计划需求回流主版本，添加弹窗显示具体的异常原因

--- src/frontend/devops-repository/src/views/planManage/planLog.vue
+++ src/frontend/devops-repository/src/views/planManage/planLog.vue
@@ -1,57 +1,75 @@
 <template>
-    <bk-sideslider :is-show.sync="showSideslider" :quick-close="true" :width="800" :title="`${planData.name} 执行任务记录`">
-        <template #content>
-            <div class="plan-detail-container" v-bkloading="{ isLoading }">
-                <bk-radio-group
-                    v-if="planData.replicaType !== 'REAL_TIME'"
-                    class="mt10 pr20"
-                    style="text-align: right;"
-                    v-model="status"
-                    @change="handlerPaginationChange()">
-                    <bk-radio class="ml50" value="">全部</bk-radio>
-                    <bk-radio class="ml50" value="SUCCESS">成功</bk-radio>
-                    <bk-radio class="ml50" value="FAILED">失败</bk-radio>
-                </bk-radio-group>
-                <bk-table
-                    class="mt10"
-                    :height="planData.replicaType !== 'REAL_TIME' ? 'calc(100% - 90px)' : 'calc(100% - 60px)'"
-                    :data="logList"
-                    :outer-border="false"
-                    :row-border="false"
-                    :row-style="{ cursor: 'pointer' }"
-                    size="small"
-                    @row-click="showLogDetailHandler">
-                    <bk-table-column type="index" label="编号" width="60"></bk-table-column>
-                    <bk-table-column label="运行状态" width="90">
-                        <template #default="{ row }">
-                            <span class="repo-tag" :class="row.status">{{asyncPlanStatusEnum[row.status] || '未执行'}}</span>
-                        </template>
-                    </bk-table-column>
-                    <bk-table-column label="开始时间" width="150" show-overflow-tooltip>
-                        <template #default="{ row }">{{formatDate(row.startTime)}}</template>
-                    </bk-table-column>
-                    <bk-table-column v-if="planData.replicaType !== 'REAL_TIME'" label="结束时间" width="150" show-overflow-tooltip>
-                        <template #default="{ row }">{{formatDate(row.endTime)}}</template>
-                    </bk-table-column>
-                    <bk-table-column label="备注" show-overflow-tooltip>
-                        <template #default="{ row }">{{row.errorReason || '/'}}</template>
-                    </bk-table-column>
-                </bk-table>
-                <bk-pagination
-                    class="p10"
-                    size="small"
-                    align="right"
-                    show-total-count
-                    @change="current => handlerPaginationChange({ current })"
-                    @limit-change="limit => handlerPaginationChange({ limit })"
-                    :current.sync="pagination.current"
-                    :limit="pagination.limit"
-                    :count="pagination.count"
-                    :limit-list="pagination.limitList">
-                </bk-pagination>
+    <div>
+        <bk-sideslider :is-show.sync="showSideslider" :quick-close="true" :width="600" :title="`${planData.name} 执行任务记录`">
+            <template #content>
+                <div class="plan-detail-container" v-bkloading="{ isLoading }">
+                    <bk-radio-group
+                        v-if="planData.replicaType !== 'REAL_TIME'"
+                        class="mt10 pr20"
+                        style="text-align: right;"
+                        v-model="status"
+                        @change="handlerPaginationChange()">
+                        <bk-radio class="ml50" value="">全部</bk-radio>
+                        <bk-radio class="ml50" value="SUCCESS">成功</bk-radio>
+                        <bk-radio class="ml50" value="FAILED">失败</bk-radio>
+                    </bk-radio-group>
+                    <bk-table
+                        class="mt10"
+                        :height="planData.replicaType !== 'REAL_TIME' ? 'calc(100% - 90px)' : 'calc(100% - 60px)'"
+                        :data="logList"
+                        :outer-border="false"
+                        :row-border="false"
+                        :row-style="{ cursor: 'pointer' }"
+                        size="small"
+                        @row-click="showLogDetailHandler">
+                        <bk-table-column type="index" label="编号" width="70"></bk-table-column>
+                        <bk-table-column label="运行状态">
+                            <template #default="{ row }">
+                                <span class="repo-tag" :class="row.status">{{asyncPlanStatusEnum[row.status] || '未执行'}}</span>
+                            </template>
+                        </bk-table-column>
+                        <bk-table-column label="开始时间" width="150" show-overflow-tooltip>
+                            <template #default="{ row }">{{formatDate(row.startTime)}}</template>
+                        </bk-table-column>
+                        <bk-table-column v-if="planData.replicaType !== 'REAL_TIME'" label="结束时间" width="150" show-overflow-tooltip>
+                            <template #default="{ row }">{{formatDate(row.endTime)}}</template>
+                        </bk-table-column>
+                        <bk-table-column :label="$t('planLogErrorReason')" width="120">
+                            <template #default="{ row }">
+                                <bk-button v-if="row.errorReason" style="width:80%;" text theme="primary" @click.stop="openErrorDetail(row)">查看</bk-button>
+                                <span v-else>/</span>
+                            </template>
+                        </bk-table-column>
+                    </bk-table>
+                    <bk-pagination
+                        class="p10"
+                        size="small"
+                        align="right"
+                        show-total-count
+                        @change="current => handlerPaginationChange({ current })"
+                        @limit-change="limit => handlerPaginationChange({ limit })"
+                        :current.sync="pagination.current"
+                        :limit="pagination.limit"
+                        :count="pagination.count"
+                        :limit-list="pagination.limitList">
+                    </bk-pagination>
+                </div>
+            </template>
+        </bk-sideslider>
+        <canway-dialog
+            v-model="errorDialog.show"
+            width="450"
+            height-num="380"
+            :title="$t('planLogErrorReason')"
+            @cancel="closeErrorDialog">
+            <div class="error-dialog-info">
+                {{errorDialog.reason}}
             </div>
-        </template>
-    </bk-sideslider>
+            <template #footer>
+                <bk-button @click="closeErrorDialog">{{$t('close')}}</bk-button>
+            </template>
+        </canway-dialog>
+    </div>
 </template>
 <script>
     import { formatDate } from '@repository/utils'
@@ -81,6 +99,10 @@
                     current: 1,
                     limit: 20,
                     limitList: [10, 20, 40]
+                },
+                errorDialog: {
+                    show: false,
+                    reason: ''
                 }
             }
         },
@@ -146,6 +168,18 @@
                         }
                     })
                 }
+            },
+            openErrorDetail (row) {
+                this.errorDialog = {
+                    show: true,
+                    reason: row.errorReason
+                }
+            },
+            closeErrorDialog () {
+                this.errorDialog = {
+                    show: false,
+                    reason: ''
+                }
             }
         }
     }
@@ -153,5 +187,9 @@
 <style lang="scss" scoped>
 .plan-detail-container {
     height: 100%;
+}
+.error-dialog-info{
+    max-height: 250px;
+    overflow-y: auto;
 }
 </style>

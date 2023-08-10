ce3a396ec697edbfb12bd7c20666c5d38e25924f
谷俊崎[junqigu] <junqigu@canway.net>
2023-08-10 11:33:55 +0800

    feat: p230_386 制品搜索的制品类型修改为下拉框样式，并去除选中的相关样式

--- src/frontend/devops-repository/src/views/repoSearch/typeSelect.vue
+++ src/frontend/devops-repository/src/views/repoSearch/typeSelect.vue
@@ -1,22 +1,20 @@
 <template>
+    <!-- 制品类型下拉筛选项 -->
     <div class="type-select-container flex-center"
         :class="{ 'active': showDropdown }"
         @click="showDropdown = !showDropdown"
         v-bk-clickoutside="hiddenDropdown">
-        <div class="flex-column flex-center">
-            <Icon size="24" :name="repoType" />
-            <span style="margin-top: -5px;">{{repoType}}</span>
+        <div class="flex-align-center">
+            <Icon size="20" :name="repoType" />
+            <span class="ml5">{{repoType}}</span>
         </div>
-        <i class="ml10 devops-icon" :class="showDropdown ? 'icon-angle-up' : 'icon-angle-down'"></i>
+        <i class="ml5 devops-icon" :class="showDropdown ? 'icon-angle-up' : 'icon-angle-down'"></i>
         <div v-show="showDropdown" class="dropdown-list" @click.stop="() => {}">
             <bk-radio-group :value="repoType" class="repo-type-radio-group" @change="changeType">
                 <bk-radio-button v-for="repo in repoList" :key="repo" :value="repo">
-                    <div class="flex-column flex-center repo-type-radio" :class="{ 'checked': repo === repoType }">
-                        <Icon size="32" :name="repo" />
-                        <span>{{repo}}</span>
-                        <span v-show="repo === repoType" class="top-right-selected">
-                            <i class="devops-icon icon-check-1"></i>
-                        </span>
+                    <div class="flex-align-center repo-type-radio" :class="{ 'checked': repo === repoType }">
+                        <Icon size="20" :name="repo" />
+                        <span class="ml10">{{repo}}</span>
                     </div>
                 </bk-radio-button>
             </bk-radio-group>
@@ -55,7 +53,7 @@
 <style lang="scss" scoped>
 .type-select-container {
     position: relative;
-    width: 111px;
+    width: 120px;
     height: 48px;
     margin-right: -1px;
     border-radius: 2px 0 0 2px;
@@ -77,45 +75,36 @@
         position: absolute;
         top: calc(100% + 10px);
         left: 0;
-        width: 580px;
-        padding: 30px;
+        width: 120px;
+        max-height: 216px;
+        overflow-y: auto;
+        padding: 5px 10px;
         background-color: white;
         box-shadow: 0px 0px 6px 0px rgba(167, 167, 167, 0.5);
         z-index: 1;
         cursor: default;
         .repo-type-radio-group {
             display: grid;
-            grid-template: auto / repeat(5, 88px);
-            gap: 20px;
+            grid-template: auto / repeat(1, 50px);
+            gap: 6px;
+            justify-items: start;
             ::v-deep .bk-form-radio-button {
                 .bk-radio-button-text {
-                    height: auto;
+                    height: 100%;
                     line-height: initial;
                     padding: 0;
-                    border-radius: 2px;
+                    border: none;
                 }
             }
             .repo-type-radio {
                 position: relative;
-                padding: 5px;
-                width: 88px;
-                height: 66px;
+                padding: 0 0 0 5px;
+                width: 100px;
+                height: 30px;
                 &.checked {
+                    height: 30px;
                     background-color: var(--bgHoverLighterColor);
                     color: var(--primaryColor) ;
-                }
-                .top-right-selected {
-                    position: absolute;
-                    top: 0;
-                    right: 0;
-                    border-width: 16px;
-                    border-style: solid;
-                    border-color: var(--primaryColor) var(--primaryColor) transparent transparent;
-                    i {
-                        position: absolute;
-                        margin-top: -12px;
-                        color: #fff;
-                    }
                 }
             }
         }

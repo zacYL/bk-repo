a1356535cf7773935fd4c94f81506549232f5140
谷俊崎[junqigu] <junqigu@canway.net>
2023-09-12 19:06:37 +0800

    fix: p230_651 软件源模式下在制品版本详情页点击面包屑回退时出现接口报错问题

--- src/frontend/devops-software/src/store/extStore.js
+++ src/frontend/devops-software/src/store/extStore.js
@@ -150,7 +150,7 @@ export default {
                 'repository/api/software/repo/list',
                 {
                     params: {
-                        type: type || ''
+                        ...(type || '')
                     }
                 }
             )

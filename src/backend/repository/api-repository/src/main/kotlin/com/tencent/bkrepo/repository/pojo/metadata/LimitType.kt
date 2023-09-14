46edb57a4e78997225cbec5d7efbb87449aa7950
禤嘉俊[ericxuan] <ericxuan@canway.net>
2023-09-14 10:49:45 +0800

    feat :p230_532 禁止操作增加元数据；

---
+++ src/backend/repository/api-repository/src/main/kotlin/com/tencent/bkrepo/repository/pojo/metadata/LimitType.kt
@@ -0,0 +1,8 @@
+package com.tencent.bkrepo.repository.pojo.metadata
+
+enum class LimitType {
+    // 禁用
+    FORBID,
+    // 锁定
+    LOCK
+}
---
+++ src/backend/repository/api-repository/src/main/kotlin/com/tencent/bkrepo/repository/pojo/metadata/LockType.kt
@@ -0,0 +1,9 @@
+package com.tencent.bkrepo.repository.pojo.metadata
+
+/**
+ * 制品锁定类型
+ */
+enum class LockType {
+    // 手动锁定
+    MANUAL
+}

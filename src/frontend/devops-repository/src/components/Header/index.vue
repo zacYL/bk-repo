b9fb0333a208836c20e88ded4c1058c1ad6cb06d
谷俊崎[junqigu] <junqigu@canway.net>
2023-10-09 10:21:04 +0800

    feat: p230_857 产品国际化，独立部署模式下添加切换语言的操作按钮

--- src/frontend/devops-repository/src/components/Header/index.vue
+++ src/frontend/devops-repository/src/components/Header/index.vue
@@ -32,15 +32,63 @@
                 </bk-option>
             </bk-select>
         </div>
+        <div class="language-select">
+            <bk-popover
+                theme="light"
+                placement="bottom"
+                :arrow="false"
+                trigger="click">
+                <div class="flag-box">
+                    <Icon :name="curLang.icon" size="26" />
+                </div>
+                <template slot="content">
+                    <li
+                        v-for="(item, index) in icons"
+                        :key="index"
+                        class="bkci-dropdown-item"
+                        @click="changeLanguage(item.id)">
+                        <Icon class="mr5" :name="item.icon" size="26" />
+                        {{item.name}}
+                    </li>
+                </template>
+            </bk-popover>
+        </div>
         <User />
     </div>
 </template>
 <script>
     import User from '@repository/components/User'
     import { mapState, mapActions } from 'vuex'
+    import cookies from 'js-cookie'
     export default {
         name: 'bkrepoHeader',
         components: { User },
+        props: {
+            icons: {
+                type: Array,
+                default: () => [
+                    {
+                        icon: 'english',
+                        name: 'English',
+                        id: 'en'
+                    },
+                    {
+                        icon: 'chinese',
+                        name: '中文',
+                        id: 'zh-cn'
+                    }
+                ]
+            }
+        },
+        data () {
+            return {
+                language: '',
+                curLang: {
+                    id: '',
+                    icon: ''
+                }
+            }
+        },
         computed: {
             ...mapState(['projectList']),
             projectId () {
@@ -49,6 +97,10 @@
             title () {
                 return document.title
             }
+        },
+        created () {
+            this.language = cookies.get('blueking_language') || 'zh-cn'
+            this.curLang = this.icons.find(item => item.id === this.language) || { id: 'zh-cn', icon: 'chinese' }
         },
         methods: {
             ...mapActions(['checkPM']),
@@ -63,6 +115,18 @@
                     },
                     query: this.$route.query
                 })
+            },
+            changeLanguage (id) {
+                const BK_CI_DOMAIN = location.host.split('.').slice(1).join('.')
+                if (id !== 'zh-cn') {
+                    cookies.remove('blueking_language', { domain: BK_CI_DOMAIN, path: '/' })
+                    cookies.set('blueking_language', 'en', { domain: BK_CI_DOMAIN, path: '/' })
+                    location.reload()
+                } else {
+                    cookies.remove('blueking_language', { domain: BK_CI_DOMAIN, path: '/' })
+                    cookies.set('blueking_language', 'zh-cn', { domain: BK_CI_DOMAIN, path: '/' })
+                    location.reload()
+                }
             }
         }
     }
@@ -73,10 +137,10 @@
     padding: 0 20px;
     justify-content: space-between;
     background-color:  var(--fontPrimaryColor);
-    color: white;
+    color: #fff;
     .bkre-project-select {
         width: 300px;
-        color: white;
+        color: #fff;
         border-color: #FFFFFF33;
         background-color: #FFFFFF1A;
         &:hover {
@@ -84,11 +148,11 @@
         }
 
         ::v-deep .bk-select-name {
-            color: white;
+            color: #fff;
         }
     }
     .bkrepo-logo {
-        color: white;
+        color: #fff;
     }
     .bkrepo-title {
         font-size: 18px;
@@ -96,7 +160,7 @@
     }
     .link {
         padding: 0 10px;
-        color: white;
+        color: #fff;
         line-height: 24px;
         border: 1px solid #FFFFFF33;
         border-radius: 2px;
@@ -109,6 +173,47 @@
         &:hover {
             background-color: rgba(255, 255, 255, 0.4);
         }
+    }
+    .language-select:hover{
+        cursor: pointer;
+    }
+    .language-select {
+        margin-left: auto;
+    }
+}
+.flag-box{
+    border-radius:20px;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover {
+        background-color: #253146;
+    }
+}
+.bkci-dropdown-item {
+    display: flex;
+    align-items: center;
+    height: 32px;
+    line-height: 32px;
+    padding: 0 8px;
+    color: #63656e;
+    font-size: 12px;
+    text-decoration: none;
+    white-space: nowrap;
+    background-color: #fff;
+    cursor: pointer;
+    &:hover {
+        background-color: #EAF3FF;
+        color: #6BA3FF;
+    }
+    &.disabled {
+        color: #dcdee5;
+        cursor: not-allowed;
+    }
+    &.active {
+        background-color: #f5f7fb;
     }
 }
 </style>

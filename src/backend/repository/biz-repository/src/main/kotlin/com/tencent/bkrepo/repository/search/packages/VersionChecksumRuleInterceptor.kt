5198adc0982bb7c07384361ed77cb8a56e937a01
ericwu <ericwu@canway.net>
2023-10-28 18:34:11 +0800

    refactor: p230_1011 自定义checksum搜索支持oci仓库

--- src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/search/packages/VersionChecksumRuleInterceptor.kt
+++ src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/search/packages/VersionChecksumRuleInterceptor.kt
@@ -1,8 +1,8 @@
 package com.tencent.bkrepo.repository.search.packages
 
-import com.tencent.bkrepo.common.api.constant.StringPool
 import com.tencent.bkrepo.common.api.exception.ErrorCodeException
 import com.tencent.bkrepo.common.api.message.CommonMessageCode
+import com.tencent.bkrepo.common.artifact.pojo.RepositoryType
 import com.tencent.bkrepo.common.query.enums.OperationType
 import com.tencent.bkrepo.common.query.model.Rule
 import com.tencent.bkrepo.common.security.util.SecurityUtils
@@ -10,7 +10,6 @@ import com.tencent.bkrepo.repository.dao.NodeDao
 import com.tencent.bkrepo.repository.dao.PackageVersionDao
 import com.tencent.bkrepo.repository.model.TNode
 import com.tencent.bkrepo.repository.model.TPackageVersion
-import com.tencent.bkrepo.repository.pojo.packages.PackageType
 import com.tencent.bkrepo.repository.pojo.repo.RepoListOption
 import com.tencent.bkrepo.repository.service.repo.RepositoryService
 import org.springframework.data.mongodb.core.query.Criteria
@@ -21,8 +20,6 @@ import org.springframework.stereotype.Component
 
 /**
  * 版本Checksum规则拦截器, 查询包版本Checksum需要在嵌套查询规则列表中指定projectId和repoType条件，且均为EQ操作
- *
- * 注意: Docker切换为Oci后需要更改这部分逻辑
  */
 @Component
 class VersionChecksumRuleInterceptor(
@@ -41,21 +38,26 @@ class VersionChecksumRuleInterceptor(
                 throw ErrorCodeException(CommonMessageCode.METHOD_NOT_ALLOWED, "$field only support EQ operation type.")
             }
             val projectId = context.findProjectId()
-            val repoType = context.findRepoType()
+            val repoType = context.findRepoType().toUpperCase()
             val userId = SecurityUtils.getUserId()
-            val repoList =
-                repositoryService.listPermissionRepo(userId, projectId, RepoListOption(type = repoType)).map { it.name }
+            val repoList = if (repoType in listOf(RepositoryType.DOCKER.name, RepositoryType.OCI.name)) {
+                val dockerRepoList = repositoryService.listPermissionRepo(
+                    userId, projectId, RepoListOption(type = RepositoryType.DOCKER.name)
+                )
+                val ociRepoList = repositoryService.listPermissionRepo(
+                    userId, projectId, RepoListOption(type = RepositoryType.OCI.name)
+                )
+                dockerRepoList + ociRepoList
+            } else
+                repositoryService.listPermissionRepo(userId, projectId, RepoListOption(type = repoType))
             val nodeQuery = Query(
                 Criteria.where(TNode::projectId.name).isEqualTo(projectId)
-                    .and(TNode::repoName.name).inValues(repoList)
+                    .and(TNode::repoName.name).inValues(repoList.map { it.name })
                     .and(field).isEqualTo(value.toString())
             )
             val fullPaths = queryRecords(nodeQuery) { q -> nodeDao.find(q) }.map { it.fullPath }
-            // Docker切换为Oci后需要更改这部分逻辑
-            return if (repoType.equals(PackageType.DOCKER.name, ignoreCase = true)) {
-                Criteria.where(TPackageVersion::manifestPath.name).inValues(
-                    fullPaths.map { it.substringBeforeLast(StringPool.SLASH) + DOCKER_MANIFEST_SUFFIX }
-                )
+            return if (repoType == RepositoryType.DOCKER.name || repoType == RepositoryType.OCI.name) {
+                Criteria.where(TPackageVersion::manifestPath.name).inValues(fullPaths)
             } else {
                 Criteria.where(TPackageVersion::artifactPath.name).inValues(fullPaths)
             }
@@ -63,7 +65,6 @@ class VersionChecksumRuleInterceptor(
     }
 
     companion object {
-        private const val DOCKER_MANIFEST_SUFFIX = "/manifest.json"
         private val CHECKSUM_FIELDS = arrayOf(
             TNode::sha256.name,
             TNode::md5.name

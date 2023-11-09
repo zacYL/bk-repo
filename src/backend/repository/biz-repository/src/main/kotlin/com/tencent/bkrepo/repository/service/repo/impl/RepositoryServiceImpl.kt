d1d55cd7488558faa49dc7cd7604121500498896
ericwu <ericwu@canway.net>
2023-11-09 10:31:49 +0800

    feature: p230_1111 oci远程仓库测试连接二次认证

--- src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/service/repo/impl/RepositoryServiceImpl.kt
+++ src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/service/repo/impl/RepositoryServiceImpl.kt
@@ -29,11 +29,13 @@ package com.tencent.bkrepo.repository.service.repo.impl
 
 import com.tencent.bkrepo.auth.api.ServicePermissionResource
 import com.tencent.bkrepo.common.api.constant.DEFAULT_PAGE_SIZE
+import com.tencent.bkrepo.common.api.constant.HttpHeaders
 import com.tencent.bkrepo.common.api.constant.StringPool
 import com.tencent.bkrepo.common.api.constant.HttpStatus
 import com.tencent.bkrepo.common.api.exception.ErrorCodeException
 import com.tencent.bkrepo.common.api.message.CommonMessageCode
 import com.tencent.bkrepo.common.api.pojo.Page
+import com.tencent.bkrepo.common.api.util.AuthenticationUtil
 import com.tencent.bkrepo.common.api.util.Preconditions
 import com.tencent.bkrepo.common.api.util.readJsonString
 import com.tencent.bkrepo.common.api.util.toJsonString
@@ -111,6 +113,7 @@ import org.springframework.data.mongodb.core.query.where
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.net.SocketTimeoutException
+import java.net.URL
 import java.net.UnknownHostException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -686,7 +689,30 @@ class RepositoryServiceImpl(
             credentials = remoteUrlRequest.credentials,
             network = remoteUrlRequest.network
         )
+        val repoType = remoteUrlRequest.type?.toUpperCase()
         return try {
+            if (repoType == RepositoryType.DOCKER.name || repoType == RepositoryType.OCI.name) {
+                val baseUrl = URL(remoteConfiguration.url)
+                val v2Url = URL(baseUrl, "/v2/")
+                val v2Config = RemoteConfiguration(
+                    url = v2Url.toString(),
+                    network = remoteUrlRequest.network
+                )
+                logger.info("oci challenge v2 endpoint: $v2Url")
+                val v2Response = remoteRepository.getResponse(v2Config)
+                val v2Code = v2Response.code()
+                val v2Reason = HttpStatus.valueOf(v2Code).reasonPhrase
+                if (v2Code == 401) {
+                    val wwwAuthenticate = v2Response.header(HttpHeaders.WWW_AUTHENTICATE)
+                    val authProperty = AuthenticationUtil.parseWWWAuthenticateHeader(wwwAuthenticate!!, "*")
+                    val urlStr =
+                        AuthenticationUtil.buildAuthenticationUrl(authProperty!!, remoteUrlRequest.credentials.username)
+                    logger.info("oci authentication url: $urlStr")
+                    remoteConfiguration.url = urlStr!!
+                } else {
+                    return ConnectionStatusInfo(v2Response.code() < 400, "${v2Response.code()} $v2Reason")
+                }
+            }
             val response = remoteRepository.getResponse(remoteConfiguration)
             val reason = HttpStatus.valueOf(response.code()).reasonPhrase
             ConnectionStatusInfo(response.code() < 400, "${response.code()} $reason")

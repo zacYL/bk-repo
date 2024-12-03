a3b96525d34342e598441f5f5933c0d779a36a58
xiaoshanwen <xiaoshanwen@canway.net>
2024-12-03 14:03:48 +0800

    feat: p230_2133 【CPack 提升专项】新增制品库类型Cocoapods，本期只做本地仓库 前端仓库接入 使用说明细节优化

--- src/frontend/devops-repository/src/views/repoCommon/repoGuideMixin.js
+++ src/frontend/devops-repository/src/views/repoCommon/repoGuideMixin.js
@@ -1544,7 +1544,7 @@ export default {
                         {
                             subTitle: this.$t('cocoapodsCreditGuideSubTitle3'),
                             codeList: [
-                                `curl -u ${this.userName}:${this.accessToken} -XPUT ${this.repoName} ${location.origin}/${this.repoType}/${this.projectId}/${this.repoName}/${this.dependInputValue1 || '<TARGET_FILE_PATH>'} -T ${this.dependInputValue2 || '<PATH_TO_FILE>'}`
+                                `curl -u${this.userName}:${this.accessToken} -XPUT ${location.origin}/${this.repoType}/${this.projectId}/${this.repoName}/${this.dependInputValue1 || '<TARGET_FILE_PATH>'} -T ${this.dependInputValue2 || '<PATH_TO_FILE>'}`
                             ]
                         }
                     ]

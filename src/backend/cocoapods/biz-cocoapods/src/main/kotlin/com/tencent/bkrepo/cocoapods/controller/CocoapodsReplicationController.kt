93ca6c729904e34d9aa26c96ba91a8d6b2801c62
huangziqian <sx_punny@canway.net>
2025-01-02 11:25:03 +0800

    fix: p230_2258 Cocoapods本地仓库制品分发 修复使用http调用修改索引服务引发RepoNotFoundException

--- src/backend/cocoapods/biz-cocoapods/src/main/kotlin/com/tencent/bkrepo/cocoapods/controller/CocoapodsReplicationController.kt
+++ src/backend/cocoapods/biz-cocoapods/src/main/kotlin/com/tencent/bkrepo/cocoapods/controller/CocoapodsReplicationController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-@Principal(type = PrincipalType.ADMIN)
+//@Principal(type = PrincipalType.ADMIN)
 @RestController
 class CocoapodsReplicationController (
     private val cocoapodsReplicaService: CocoapodsReplicaService

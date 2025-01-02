f6a59f1593477ac8e1fbd9ed844174b804ae1d27
huangziqian <sx_punny@canway.net>
2025-01-02 11:13:24 +0800

    fix: p230_2258 Cocoapods本地仓库制品分发 修复使用http调用修改索引服务引发RepoNotFoundException

--- src/backend/cocoapods/biz-cocoapods/src/main/kotlin/com/tencent/bkrepo/cocoapods/controller/CocoapodsReplicationController.kt
+++ src/backend/cocoapods/biz-cocoapods/src/main/kotlin/com/tencent/bkrepo/cocoapods/controller/CocoapodsReplicationController.kt
@@ -3,8 +3,11 @@ package com.tencent.bkrepo.cocoapods.controller;
 import com.tencent.bkrepo.cocoapods.service.CocoapodsReplicaService
 import com.tencent.bkrepo.common.artifact.event.base.ArtifactEvent;
 import com.tencent.bkrepo.cocoapods.constant.COCOAPODS_REPLICA_RESOLVE
+import com.tencent.bkrepo.cocoapods.event.consumer.RemoteEventJobExecutor
 import com.tencent.bkrepo.common.security.permission.Principal
 import com.tencent.bkrepo.common.security.permission.PrincipalType
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +21,13 @@ class CocoapodsReplicationController (
      * 外部通过http进行调用
      */
     @PostMapping(COCOAPODS_REPLICA_RESOLVE)
-    fun resolve(@RequestBody event:ArtifactEvent){
+    fun resolve(@RequestBody event:ArtifactEvent,
+                @PathVariable projectId: String,
+                @PathVariable repoName: String,){
+        logger.info("resolveIndexFile form http:projectId:$projectId, repoName:$repoName")
         cocoapodsReplicaService.resolveIndexFile(event)
+    }
+    companion object {
+        private val logger = LoggerFactory.getLogger(RemoteEventJobExecutor::class.java)
     }
 }
